3ed6f06a930ca8fbe311944e51b80b5a03bcf3ee
Code <139698754+Nayenso@users.noreply.github.com>
2024-09-26 22:46:28 +0900

    Title: 데이터 교환, Time: 86ms, Memory: 8MB, Status: Passed - CodeTree

--- 240926/데이터 교환/exchange-data.java
+++ 240926/데이터 교환/exchange-data.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         int a = 5, b = 6, c = 7, temp;
-        temp = a;
-        a = b;
-        b = c;
-        c = temp;
+        temp = c;
+        c = b;
+        b = a;
+        a = temp;
         System.out.print(a+"\n"+b+"\n"+c);
     }
 }
