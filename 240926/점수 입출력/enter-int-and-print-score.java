d9d89250c47e9bd06cfc33dded385f94ad8ae7d2
Code <139698754+Nayenso@users.noreply.github.com>
2024-09-26 23:04:20 +0900

    Title: 점수 입출력, Time: 151ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240926/점수 입출력/enter-int-and-print-score.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+
+        System.out.printf("Your score is %d point.",n);
+    }
+}
