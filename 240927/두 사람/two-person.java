f0235882ddf4d99e45ef79ebd4a9bee366833a9f
Code <139698754+Nayenso@users.noreply.github.com>
2024-09-27 21:23:58 +0900

    Title: 두 사람, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240927/두 사람/two-person.java
+++ 240927/두 사람/two-person.java
@@ -4,7 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int A = input.nextInt();
+        char aS = input.next().charAt(0);
         int B = input.nextInt();
+        char BS = input.next().charAt(0);
 
         if(A >= 19 || B >= 19){
             System.out.print(1);
