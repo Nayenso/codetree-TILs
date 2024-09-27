96eaa2a729bde58b39aab351a0d8d372f6e50d1b
Code <139698754+Nayenso@users.noreply.github.com>
2024-09-27 18:18:57 +0900

    Title: 합과 평균의 차, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240927/합과 평균의 차/sub-of-average-and-sum.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int c = input.nextInt();
+        int avg = (a+b+c)/3;
+        int re = (a+b+c)=avg;
+        System.out.print(a+b+c+"\n"+avg+"\n"+re);
+    }
+}
