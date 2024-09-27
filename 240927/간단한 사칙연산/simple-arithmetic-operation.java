a6087eb527bf28e39ea53ab97405c09d8b7e340a
Code <139698754+Nayenso@users.noreply.github.com>
2024-09-27 17:56:00 +0900

    Title: 간단한 사칙연산, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240927/간단한 사칙연산/simple-arithmetic-operation.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = input.nextInt();
+
+        System.out.printf("%d\n%d\n%d\n%d",a+b,a-b,a/b,a%b);
+    }
+}
