9a3d12bcf05b177ae833f291fd142bb74c3382de
Code <139698754+Nayenso@users.noreply.github.com>
2024-09-27 18:11:57 +0900

    Title: 합과 평균, Time: 156ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240927/합과 평균/sum-and-avg.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = input.nextInt();
+        System.out.printf("%d %.1f", a+b, ((double)a + b)/2);
+    }
+}
