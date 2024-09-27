8ae5f5f189863e1ecb30edca2df57f7d131058b7
Code <139698754+Nayenso@users.noreply.github.com>
2024-09-27 20:41:29 +0900

    Title: 4번의 크기 비교, Time: 201ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240927/4번의 크기 비교/4-time-comparison.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int c = input.nextInt();
+        int d = input.nextInt();
+        int e = input.nextInt();
+        System.out.println(a>b?1:0);
+        System.out.println(a>c?1:0);
+        System.out.println(a>d?1:0);
+        System.out.println(a>e?1:0);
+    }
+}
