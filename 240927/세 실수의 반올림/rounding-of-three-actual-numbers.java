d485abf088fbd111083426b0131436654af4cd44
Code <139698754+Nayenso@users.noreply.github.com>
2024-09-27 16:51:03 +0900

    Title: 세 실수의 반올림, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240927/세 실수의 반올림/rounding-of-three-actual-numbers.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double a = input.nextDouble();
+        double b = input.nextDouble();
+        double c = input.nextDouble();
+
+        Systme.out.printf("%.3f\n%.3f\n%.3f",a,b,c);
+    }
+}
