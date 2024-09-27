6d25f756174ac80382ccb3cacb07c200f7f00349
Code <139698754+Nayenso@users.noreply.github.com>
2024-09-27 18:40:08 +0900

    Title: 삼항연산자 2, Time: 239ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240927/삼항연산자 2/ternary-operator-2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        char re = (a==1)?'t':'f';
+        System.out.print(re);
+    }
+}
