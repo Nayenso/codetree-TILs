962ebe393e1512c16b9548dac3e100bbaa0e5506
Code <139698754+Nayenso@users.noreply.github.com>
2024-09-27 17:18:38 +0900

    Title: 입력받아 출력 2, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240927/입력받아 출력 2/input-and-output-2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        input.useDelimiter("-");
+        int front = input.nextInt();
+        int rear = input.nextInt();
+        System.out.print(front+""+rear);
+    }
+}
