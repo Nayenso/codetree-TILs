b65400623d4bbea37ae6aa6cf788e3364d1b6f65
Code <139698754+Nayenso@users.noreply.github.com>
2024-09-27 17:27:13 +0900

    Title: 전화번호 바꾸기, Time: 119ms, Memory: 10MB, Status: Passed - CodeTree

--- 240927/전화번호 바꾸기/change-phone-number.java
+++ 240927/전화번호 바꾸기/change-phone-number.java
@@ -4,9 +4,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         input.useDelimiter("-");
+
         int front = input.nextInt();
         int mid = input.nextInt();
         int rear = input.nextInt();
-        System.out.print(front+"-"+rear+"-"+mid);
+
+        System.out.print(0+""+front+"-"+rear+"-"+mid);
     }
 }
