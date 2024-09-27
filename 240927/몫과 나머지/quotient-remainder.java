ee551316eb82e844b45f0482470bdc707239e955
Code <139698754+Nayenso@users.noreply.github.com>
2024-09-27 18:01:32 +0900

    Title: 몫과 나머지, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240927/몫과 나머지/quotient-remainder.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(Systme.in);
+        int a = input.nextInt();
+        int b = input.nextInt();
+        System.out.print(a/b +"..."+a%b);
+    }
+}
