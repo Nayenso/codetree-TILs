cd635d40dca1248904bbb11bb9ef00e8e1e91eba
Code <139698754+Nayenso@users.noreply.github.com>
2024-09-27 19:01:12 +0900

    Title: 숫자의 조건 여부 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240927/숫자의 조건 여부 2/numbers-condition-2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        if(a==5){
+            System.out.print("A");
+        }
+                if(a%2==0){
+            System.out.print("B");
+        }
+    }
+}
