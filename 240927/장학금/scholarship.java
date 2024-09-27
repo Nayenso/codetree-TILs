2f1ac00332ddea57f164545bd90c7c3def2380ec
Code <139698754+Nayenso@users.noreply.github.com>
2024-09-27 21:15:15 +0900

    Title: 장학금, Time: 114ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240927/장학금/scholarship.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int mid = input.nextInt();
+        int fin = input.nextInt();
+
+        if(mid >= 90){
+            if(fin >= 95){
+                System.out.print(100000);
+            }else if(fin >= 90){
+                System.out.print(50000);
+            }else{
+                System.out.print(0);
+            }
+        }
+    }
+}
