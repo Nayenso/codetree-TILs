51b07d1f197f31ed0792d01bb0ba65e81c31bbce
Code <139698754+Nayenso@users.noreply.github.com>
2024-09-27 16:57:57 +0900

    Title: 문자열 받아 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240927/문자열 받아 출력/enter-word-and-print.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanenr input = new Scanner(System.in);
+        String str = input.next();
+        System.out.print(str);
+    }
+}
