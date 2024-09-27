ef79347538338837ea6bcfdc61af88b35f6ed96d
Code <139698754+Nayenso@users.noreply.github.com>
2024-09-27 18:45:26 +0900

    Title: 시력 검사 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240927/시력 검사 2/eye-test-2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double eyes = input.nextDouble();
+        if(eyes >= 1.0){
+            System.out.print("High");
+        }else if(eyes >= 0.5){
+            System.out.pirnt("Middle");
+        }else{
+            System.out.print("Low");
+        }
+    }
+}
