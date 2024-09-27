2282297c53aae331721e3247b1223ca69d51fc9e
Code <139698754+Nayenso@users.noreply.github.com>
2024-09-27 21:04:03 +0900

    Title: 범위 밖의 수, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240927/범위 밖의 수/number-out-of-range.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+
+        if(a<10||a>20){
+            System.out.print("yes");
+        }else{
+            System.out.print("no");
+        }
+    }
+}
