b9860a3f5358bb07d4ec54caae7cfdb29402ebfb
Code <139698754+Nayenso@users.noreply.github.com>
2024-09-27 19:01:51 +0900

    Title: 숫자의 조건 여부 2, Time: 239ms, Memory: 10MB, Status: Passed - CodeTree

--- 240927/숫자의 조건 여부 2/numbers-condition-2.java
+++ 240927/숫자의 조건 여부 2/numbers-condition-2.java
@@ -3,10 +3,11 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
         if(a==5){
             System.out.print("A");
         }
-                if(a%2==0){
+        if(a%2==0){
             System.out.print("B");
         }
     }
