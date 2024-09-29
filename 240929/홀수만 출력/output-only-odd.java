5d093c089e07a12947ef97fd9ab34a3d505ac7cf
Code <139698754+Nayenso@users.noreply.github.com>
2024-09-29 21:38:05 +0900

    Title: 홀수만 출력, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240929/홀수만 출력/output-only-odd.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = input.nextInt();
+
+        for(int i = a; i <= b; i++){
+            if(i%2!= 0){
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
