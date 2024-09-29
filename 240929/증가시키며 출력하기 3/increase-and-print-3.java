f6d9ea5a8c25740e2fa33b620de00ab70181dd41
Code <139698754+Nayenso@users.noreply.github.com>
2024-09-29 21:56:54 +0900

    Title: 증가시키며 출력하기 3, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240929/증가시키며 출력하기 3/increase-and-print-3.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int i = 10;
+        whlie(i<=26){
+            System.out.print(i+=" ");
+            i+=2;
+        }
+    }
+}
