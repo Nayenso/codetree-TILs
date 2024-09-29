7137bde972f7b172bf3bce596c73437fab58700f
Code <139698754+Nayenso@users.noreply.github.com>
2024-09-29 21:50:36 +0900

    Title: 별 n개 출력하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240929/별 n개 출력하기/print-n-stars.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n =input.netInt();
+        int i= 0;
+        while(i < n){
+            Sysyem.out.printin("*");
+        }
+    }
+}
