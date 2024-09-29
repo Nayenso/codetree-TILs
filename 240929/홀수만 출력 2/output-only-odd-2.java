27c240c511ea449f2ed75f658f37112b3b1688c2
Code <139698754+Nayenso@users.noreply.github.com>
2024-09-29 21:44:15 +0900

    Title: 홀수만 출력 2, Time: 131ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240929/홀수만 출력 2/output-only-odd-2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = input.nextInt();
+        for(int i = b; i >= a; i--){
+            if(i%2!=0){
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
