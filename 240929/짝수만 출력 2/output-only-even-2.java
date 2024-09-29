c1b299e2316bcf535b10cf21c413627371c91b15
Code <139698754+Nayenso@users.noreply.github.com>
2024-09-29 22:05:03 +0900

    Title: 짝수만 출력 2, Time: 3077ms, Memory: 9MB, Status: Time Limit Exceed - CodeTree

---
+++ 240929/짝수만 출력 2/output-only-even-2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int b = input.nextInt();
+        int a = input.nextInt();
+
+        while(b>=a){
+            if(b%2==0){
+                System.out.print(b--+" ");
+            }
+        }
+    }
+}
