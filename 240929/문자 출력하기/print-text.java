5a7736fbf901a7a7364243dd005967139e94c743
Code <139698754+Nayenso@users.noreply.github.com>
2024-09-29 22:08:57 +0900

    Title: 문자 출력하기, Time: 143ms, Memory: 10MB, Status: Passed - CodeTree

--- 240929/문자 출력하기/print-text.java
+++ 240929/문자 출력하기/print-text.java
@@ -1,7 +1,11 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        char word = input.next().charAt(0);
         for(int i= 0; i < 8; i++){
-            System.out.print("G");
+            System.out.print(word);
         }
     }
 }
