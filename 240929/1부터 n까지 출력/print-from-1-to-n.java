3c67cff036679dcf11ad5813ed7fc233f52ab425
Code <139698754+Nayenso@users.noreply.github.com>
2024-09-29 21:33:18 +0900

    Title: 1부터 n까지 출력, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

--- 240929/1부터 n까지 출력/print-from-1-to-n.java
+++ 240929/1부터 n까지 출력/print-from-1-to-n.java
@@ -1,11 +1,11 @@
-import java.uitl.*;
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int n = input.nextInt();
 
         for(int i = 1; i <= n; i++){
-            System.out.print(i);
+            System.out.print(i+" ");
         }
     }
 }
