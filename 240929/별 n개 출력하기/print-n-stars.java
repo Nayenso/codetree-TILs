cfede30012488bc85c05dd0e59b251eb35fd7b26
Code <139698754+Nayenso@users.noreply.github.com>
2024-09-29 21:51:02 +0900

    Title: 별 n개 출력하기, Time: 3034ms, Memory: 23MB, Status: Time Limit Exceed - CodeTree

--- 240929/별 n개 출력하기/print-n-stars.java
+++ 240929/별 n개 출력하기/print-n-stars.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int n =input.netInt();
+        int n =input.nextInt();
         int i= 0;
         while(i < n){
             System.out.println("*");
