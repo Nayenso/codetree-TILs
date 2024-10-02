7ffc0564f36eb43ec0de98d84e50cf74ff539cd7
Code <139698754+Nayenso@users.noreply.github.com>
2024-10-02 10:43:46 +0900

    Title: 배수의 개수 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241002/배수의 개수 2/number-of-multipliers-2.java
@@ -0,0 +1,14 @@
+import java.uti;.*
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int cnt = 0;
+        for(int i=0; i<10; i++){
+            int n = input.nextInt();
+            if(n%2!=0){
+                cnt++;
+            }
+        }
+        System.out.print(cnt);
+    }
+}
