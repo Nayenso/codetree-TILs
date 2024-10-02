ddaa96f402d1e6443fe00897b25cf23cbf2af30c
Code <139698754+Nayenso@users.noreply.github.com>
2024-10-02 10:56:50 +0900

    Title: 청소당번 정하기, Time: 163ms, Memory: 10MB, Status: Passed - CodeTree

--- 241002/청소당번 정하기/cleaning-numbering.java
+++ 241002/청소당번 정하기/cleaning-numbering.java
@@ -5,7 +5,7 @@ public class Main {
         Scanner input = new Scanner(System.in);
         int classRoom = 0, wall = 0, toilet = 0;
         int n = input.nextInt();
-        for(int i=1; i<n; i++){
+        for(int i=1; i<=n; i++){
             if(i%12==0){
                 toilet++;
             }else if(i%2==0 && i%3==0){
