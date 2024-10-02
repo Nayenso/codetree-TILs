f961844232ac408cca31cef2f057464a9e19cc7b
Code <139698754+Nayenso@users.noreply.github.com>
2024-10-02 11:59:52 +0900

    Title: 100부터 입력받는 수까지의 합, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241002/100부터 입력받는 수까지의 합/sum-of-the-number-input-from-100.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt(), sum = 0;
+        for(int i = n; i<=100){
+            sum += i;
+        }
+        System.out.print(sum);
+    }
+}
