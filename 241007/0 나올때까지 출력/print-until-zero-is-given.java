ac5f6bf64360a082c06c1a0cdd8c6839034bb03e
Code <139698754+Nayenso@users.noreply.github.com>
2024-10-07 12:59:35 +0900

    Title: 0 나올때까지 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241007/0 나올때까지 출력/print-until-zero-is-given.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        for(;;;){
+            int n = input.nextInt();
+            if(n==0){
+                break;
+            }
+            System.out.println(n);
+        }
+    }
+}
