63c232481f7b231ded356038169751683fe43237
Code <139698754+Nayenso@users.noreply.github.com>
2024-10-07 12:51:21 +0900

    Title: 1까지 나누기, Time: 118ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241007/1까지 나누기/divide-by-1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int cnt = 0, i = 1;
+
+        while(n/i>=1){
+            n /= i;
+            i++;
+            cnt++;
+        }
+
+        System.out.print(cnt);
+    }
+}
