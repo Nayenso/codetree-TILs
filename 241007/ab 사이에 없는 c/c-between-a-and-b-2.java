f960e278e828ddc419ec6f4e8461478f67bc5e07
Code <139698754+Nayenso@users.noreply.github.com>
2024-10-07 13:52:53 +0900

    Title: ab 사이에 없는 c, Time: 118ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241007/ab 사이에 없는 c/c-between-a-and-b-2.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        boolean flag = true;
+
+        for(int i=a; i<=b; i++){
+            if(i%c!=0){
+                flag = false;
+            }
+        }
+        if(flag = true){
+            System.out.print("YES");
+        }else{
+            System.out.print("NO");
+        }
+    }
+}
