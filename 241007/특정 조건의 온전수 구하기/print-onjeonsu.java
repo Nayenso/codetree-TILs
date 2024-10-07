1a6f67b47c73d6be1533c39815e58fb3acf6c4ff
Code <139698754+Nayenso@users.noreply.github.com>
2024-10-07 12:32:08 +0900

    Title: 특정 조건의 온전수 구하기, Time: 129ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241007/특정 조건의 온전수 구하기/print-onjeonsu.java
+++ 241007/특정 조건의 온전수 구하기/print-onjeonsu.java
@@ -4,8 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int n = input.nextInt();
+
         for(int i=1; i<=n; i++){
-            if((i%2==0) && (i%10==5) && (i%3==0 && i%9!=0)){
+            if(i%2==0 && (i%3==0 && i%9!=0) && i%10==5){
                 continue;
             }
             System.out.print(i+" ");
