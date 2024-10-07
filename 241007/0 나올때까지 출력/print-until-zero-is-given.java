cd221d338928bcb8e142213cf04f306294cb00a4
Code <139698754+Nayenso@users.noreply.github.com>
2024-10-07 12:59:44 +0900

    Title: 0 나올때까지 출력, Time: 148ms, Memory: 10MB, Status: Passed - CodeTree

--- 241007/0 나올때까지 출력/print-until-zero-is-given.java
+++ 241007/0 나올때까지 출력/print-until-zero-is-given.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        for(;;;){
+        for(;;){
             int n = input.nextInt();
             if(n==0){
                 break;
