0ca144e8e487c1ff523020fc55063d440a2b26ea
Code <139698754+Nayenso@users.noreply.github.com>
2024-10-07 13:01:33 +0900

    Title: 특정 숫자 맞추기, Time: 151ms, Memory: 10MB, Status: Passed - CodeTree

--- 241007/특정 숫자 맞추기/catching-specific-number.java
+++ 241007/특정 숫자 맞추기/catching-specific-number.java
@@ -11,6 +11,7 @@ public class Main {
                 System.out.println("Lower");
             }else{
                 System.out.print("Good");
+                break;
             }
         }
     }
