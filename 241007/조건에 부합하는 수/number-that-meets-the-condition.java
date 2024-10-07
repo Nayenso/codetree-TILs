3c0ae0ba4a81cced99a3412acaa104dfc3866edb
Code <139698754+Nayenso@users.noreply.github.com>
2024-10-07 12:40:36 +0900

    Title: 조건에 부합하는 수, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

--- 241007/조건에 부합하는 수/number-that-meets-the-condition.java
+++ 241007/조건에 부합하는 수/number-that-meets-the-condition.java
@@ -6,9 +6,16 @@ public class Main {
         int n = input.nextInt();
 
         for(int i=1; i<=n; i++){
-            if((i%2!=0 || i%4==9) || (i/8)%2!=0 || i%7>=4){
-                System.out.print(i+" ");
+            if(i%2==0 && i%4!=0){
+                continue;
             }
+            if((i/8)%2==0){
+                continue;
+            }
+            if(i%7<4){
+                continue;
+            }
+            System.out.print(i+" ");
         }
     }
 }
