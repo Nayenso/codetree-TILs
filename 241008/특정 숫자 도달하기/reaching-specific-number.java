14874130598291d4537981c503409e815492224d
Code <139698754+Nayenso@users.noreply.github.com>
2024-10-08 16:28:35 +0900

    Title: 특정 숫자 도달하기, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

--- 241008/특정 숫자 도달하기/reaching-specific-number.java
+++ 241008/특정 숫자 도달하기/reaching-specific-number.java
@@ -4,14 +4,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] num = new int[10];
-        int sum = 0;
+        int sum = 0, index = 0;
         double avg = 0;
         boolean flag = true;
 
         for(int i=0; i<10; i++){
             num[i] = sc.nextInt();
             if(num[i] >= 250){
+                index = i;
                 flag = false;
+                break;
             }
         }
 
@@ -21,10 +23,10 @@ public class Main {
             }
             avg = (double)sum/10;
         }else{
-             for(int i=0; i<9; i++){
+             for(int i=0; i<index; i++){
                 sum+=num[i];
             }
-            avg = (double)sum/9;
+            avg = (double)sum/index;
         }
         System.out.printf("%d %.1f", sum, avg);
     }
