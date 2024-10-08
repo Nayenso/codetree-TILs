ebc63a94e833f3831372531f19df7d75be94ad14
Code <139698754+Nayenso@users.noreply.github.com>
2024-10-08 16:40:55 +0900

    Title: 배열 채우기, Time: 123ms, Memory: 10MB, Status: Passed - CodeTree

--- 241008/배열 채우기/filling-array.java
+++ 241008/배열 채우기/filling-array.java
@@ -5,17 +5,24 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int[] n = new int[10];
+        boolean flag = true;
         int index = 0;
         for(int i=0; i<10; i++){
             n[i] = sc.nextInt();
             if(n[i] == 0){
                 index = i;
+                flag = false;
                 break;
             }
         }
-
-        for(int i=index-1; i>=0; i--){
-            System.out.print(n[i]+" ");
+        if(flag == true){
+            for(int i=9; i>=0; i--){
+                System.out.print(n[i]+" ");
+            }
+        }else{
+            for(int i=index-1; i>=0; i--){
+                System.out.print(n[i]+" ");
+            }
         }
     }
 }
