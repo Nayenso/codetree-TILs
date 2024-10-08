ad603581fba3d5b12a5f1297a33e6504a2c4cbc3
Code <139698754+Nayenso@users.noreply.github.com>
2024-10-08 16:38:35 +0900

    Title: 배열 채우기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241008/배열 채우기/filling-array.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanenr sc = new Scanner(System.in);
+
+        int[] n = new int[10];
+        int index = 0;
+        for(int i=0; i<10; i++){
+            n[i] = sc.nextInt();
+            if(n[i] == 0){
+                index = i;
+                break;
+            }
+        }
+
+        for(int i=index; i>=0; i--){
+            System.out.print(n[i]+" ");
+        }
+    }
+}
