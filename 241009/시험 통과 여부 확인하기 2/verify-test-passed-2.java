3e5df148275ca3f7d1425b8a56d6f8ef71ea652d
Code <139698754+Nayenso@users.noreply.github.com>
2024-10-09 11:29:40 +0900

    Title: 시험 통과 여부 확인하기 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241009/시험 통과 여부 확인하기 2/verify-test-passed-2.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=  sc.nextInt();
+        int cnt = 0;
+
+        for(int i=0; i<n; i++){
+            int[] grade = new int[4];
+            int sum = 0;
+            double avg = 0;
+            for(int j=0; j<4; j++){
+                grade[i] = sc.nextInt();
+                sum+=grade[i];
+            }
+            avg = (double)sum/4;
+            if(avg>=60){
+                System.out.println("pass");
+                cnt++;
+            }else{
+                System.out.println("fail");
+            }
+        }
+        System.out.print(cnt);
+    }
+}
