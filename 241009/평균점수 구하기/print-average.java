0fe353a037ab666122d1eff921a82ad7574b7a69
Code <139698754+Nayenso@users.noreply.github.com>
2024-10-09 11:01:33 +0900

    Title: 평균점수 구하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241009/평균점수 구하기/print-average.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scnner sc = new Scanner(System.in);
+        int sum=0;
+
+        for(int i=0; i<8; i++){
+            int n=sc.nextInt();
+            sum+=n;
+        }
+
+        System.out.printf("%.1f",(double)sum/8);
+    }
+}
