6ddaf4dc73511d6def01dcf1d94ed5c24c842d8c
Code <139698754+Nayenso@users.noreply.github.com>
2024-10-09 12:28:03 +0900

    Title: 일의 자리 배열, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241009/일의 자리 배열/array-with-units.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[10];
+        
+        int arr[0] = sc.nextInt();
+        int arr[1] = sc.nextInt();
+
+        for(int i=2; i<10; i++){
+            arr[i] = (arr[i-1]+arr[i-2])%10;
+        }
+        for(int i:arr){
+            System.out.print(i+" ");
+        }
+    }
+}
