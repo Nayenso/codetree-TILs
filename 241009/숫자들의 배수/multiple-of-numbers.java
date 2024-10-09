1676a3aa11bf8bcd4983c7c82dda288e96b71eaa
Code <139698754+Nayenso@users.noreply.github.com>
2024-10-09 12:40:05 +0900

    Title: 숫자들의 배수, Time: 146ms, Memory: 10MB, Status: Passed - CodeTree

--- 241009/숫자들의 배수/multiple-of-numbers.java
+++ 241009/숫자들의 배수/multiple-of-numbers.java
@@ -4,12 +4,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] n = new int[10];
-        int n = sc.nextInt();
+        int a = sc.nextInt();
         int cnt = 0;
 
         for(int i=1; i<=10; i++){
-            System.out.print(n*i);
-            if(n*i%5==0){
+            System.out.print(a*i+" ");
+            if((a*i)%5==0){
                 cnt++;
             }
             if(cnt==2){
