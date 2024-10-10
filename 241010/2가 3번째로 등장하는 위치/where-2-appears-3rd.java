1f09e223468c98519544fe6a499db9867e294186
Code <139698754+Nayenso@users.noreply.github.com>
2024-10-10 16:39:52 +0900

    Title: 2가 3번째로 등장하는 위치, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241010/2가 3번째로 등장하는 위치/where-2-appears-3rd.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int cnt = 0, index = 0;
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+            if(arr[i]==2){
+                cnt++;
+            }
+            if(cnt==3){
+                index= i;
+            }
+        }
+        System.out.print(i+1);
+    }
+}
