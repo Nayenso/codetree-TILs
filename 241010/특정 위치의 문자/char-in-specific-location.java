104d24b3ef1e6ba58f34e195edbee4d59c00ca99
Code <139698754+Nayenso@users.noreply.github.com>
2024-10-10 15:57:01 +0900

    Title: 특정 위치의 문자, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241010/특정 위치의 문자/char-in-specific-location.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char[] c = {'L','E','B','R','O','S'};
+        char t = sc.next().charAt();
+        int index = -1;
+
+        for(int i=0; i<6; i++){
+            if(c[i]=='t'){
+                index = i;
+            }
+        }
+        if(index == -1){
+            System.out.print("None");
+        }else{
+            System.out.print(index);
+        }
+        
+    }
+}
