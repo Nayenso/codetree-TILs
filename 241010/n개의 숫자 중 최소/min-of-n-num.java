63936aaee0641ba60e730fda032c93d210140619
Code <139698754+Nayenso@users.noreply.github.com>
2024-10-10 16:57:39 +0900

    Title: n개의 숫자 중 최소, Time: 184ms, Memory: 10MB, Status: Passed - CodeTree

--- 241010/n개의 숫자 중 최소/min-of-n-num.java
+++ 241010/n개의 숫자 중 최소/min-of-n-num.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int INT_MIN = Integer.MAX_VALUE;
+        int INT_MIN = Integer.MAX_VALUE, cnt = 0;
         int[] arr = new int[n];
 
         for(int i=0; i<n; i++){
@@ -15,7 +15,12 @@ public class Main {
                 INT_MIN=arr[i];
             }
         }
+        for(int i=0; i<n; i++){
+            if(arr[i]==INT_MIN){
+                cnt++;
+            }
+        }
 
-        System.out.print(INT_MIN);
+        System.out.print(INT_MIN+" "+cnt);
     }
 }
