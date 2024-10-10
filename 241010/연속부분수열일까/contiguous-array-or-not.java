f2dd9d1919c9002bbddeaf282e8cf7a265fb1211
Code <139698754+Nayenso@users.noreply.github.com>
2024-10-10 16:32:40 +0900

    Title: 연속부분수열일까, Time: 127ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241010/연속부분수열일까/contiguous-array-or-not.java
+++ 241010/연속부분수열일까/contiguous-array-or-not.java
@@ -25,6 +25,9 @@ public class Main {
                             break;
                         }
                     }
+                if(flag == true){
+                    break;
+                }
                 }
             }
 
