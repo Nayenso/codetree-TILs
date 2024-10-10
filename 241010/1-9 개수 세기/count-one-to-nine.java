3b203e696e50d9add333c04266f5581da6873f6f
Code <139698754+Nayenso@users.noreply.github.com>
2024-10-10 15:03:29 +0900

    Title: 1-9 개수 세기, Time: 158ms, Memory: 10MB, Status: Passed - CodeTree

--- 241010/1-9 개수 세기/count-one-to-nine.java
+++ 241010/1-9 개수 세기/count-one-to-nine.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] num = new int[9];
+        int[] num = new int[n];
         int[] index = new int[10];
 
         for(int i=0; i<n; i++){
@@ -14,6 +14,7 @@ public class Main {
         for(int i=0; i<n; i++){
             index[num[i]]++;
         }
+
         for(int i=1; i<10; i++){
             System.out.println(index[i]);
         }
