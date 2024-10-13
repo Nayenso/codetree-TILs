a4028e5a568ab185f562b2d0f78a8521a0ab92e4
Code <139698754+Nayenso@users.noreply.github.com>
2024-10-13 20:41:38 +0900

    Title: 문자열 범위 출력하기 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 241013/문자열 범위 출력하기 2/print-string-in-range-2.java
+++ 241013/문자열 범위 출력하기 2/print-string-in-range-2.java
@@ -7,7 +7,7 @@ public class Main {
         String str = sc.next();
         int n = sc.nextInt();
 
-        for(int i=str.length()-1; i>=str.length()-n-1; i--){
+        for(int i=str.length()-1; i>=str.length()-n; i--){
             System.out.print(str.charAt(i));
         }
     }
