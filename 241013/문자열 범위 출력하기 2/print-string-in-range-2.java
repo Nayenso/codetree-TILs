3ac8f6aa4de256c8338fc6570249e58513b7598f
Code <139698754+Nayenso@users.noreply.github.com>
2024-10-13 20:43:07 +0900

    Title: 문자열 범위 출력하기 2, Time: 160ms, Memory: 10MB, Status: Passed - CodeTree

--- 241013/문자열 범위 출력하기 2/print-string-in-range-2.java
+++ 241013/문자열 범위 출력하기 2/print-string-in-range-2.java
@@ -10,9 +10,11 @@ public class Main {
             for(int i=str.length()-1; i>=0; i--){
                 System.out.print(str.charAt(i));
             }
+        }else{
+            for(int i=str.length()-1; i>=str.length()-n; i--){
+                System.out.print(str.charAt(i));
+             }
         }
-        for(int i=str.length()-1; i>=str.length()-n; i--){
-            System.out.print(str.charAt(i));
-        }
+
     }
 }
