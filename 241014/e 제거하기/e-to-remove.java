1e36dfec2a63fdd0963a72eefb821991ced20838
Code <139698754+Nayenso@users.noreply.github.com>
2024-10-14 14:11:32 +0900

    Title: e 제거하기, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241014/e 제거하기/e-to-remove.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+
+        int index = str.indexOf("e");
+        str = str.substring(0,index)+str.substring(index+1);
+        System.out.print(str);
+    }
+}
