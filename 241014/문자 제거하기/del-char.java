d65420d0eae5ee5880e5ff1744417a87a5c3b151
Code <139698754+Nayenso@users.noreply.github.com>
2024-10-14 13:38:15 +0900

    Title: 문자 제거하기, Time: 167ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241014/문자 제거하기/del-char.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int len = str.length();
+
+        while(len>1){
+            int n = sc.nextInt();
+
+            if(n>=len){
+                str = str.substring(0,len-1);
+                System.out.println(str);
+                len = str.length();
+            }else{
+                str = str.substring(0,n)+str.substring(n+1);
+                System.out.println(str);
+                len = str.length();
+            }
+        }
+    }
+}
