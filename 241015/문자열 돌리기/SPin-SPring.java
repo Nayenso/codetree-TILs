ff960e88654d7d3f2f43b99120cb365175f7f525
Code <139698754+Nayenso@users.noreply.github.com>
2024-10-15 21:22:05 +0900

    Title: 문자열 돌리기, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

--- 241015/문자열 돌리기/SPin-SPring.java
+++ 241015/문자열 돌리기/SPin-SPring.java
@@ -6,9 +6,9 @@ public class Main {
 
         String str = sc.next();
         int len = str.length();
-
+        System.out.println(str);
         for(int i=0; i<len; i++){
-            str = str.substring(0,len-1) + str.substring(len-1);
+            str = str.substring(len-1)+str.substring(0,len-1);
             System.out.println(str);
         }
     }
