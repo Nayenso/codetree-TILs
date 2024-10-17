7722db1a008a71d6ae6fcf6b454be409b9ed18c9
Code <139698754+Nayenso@users.noreply.github.com>
2024-10-17 15:09:35 +0900

    Title: 그 다음 알파벳, Time: 116ms, Memory: 10MB, Status: Passed - CodeTree

--- 241017/그 다음 알파벳/next-alphabet.java
+++ 241017/그 다음 알파벳/next-alphabet.java
@@ -4,7 +4,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         char c = sc.next().charAt(0);
+        if(c=='z'){
+            System.out.print('a');
+            return;
+        }
+        char nextC = (char)((int)c+1);
 
-        System.out.print((char)((int)c+1));
+        System.out.print(nextC);
     }
 }
