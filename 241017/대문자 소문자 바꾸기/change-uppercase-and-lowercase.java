fa84db191f35854b396fdb90f6a80a9c6798fc7b
Code <139698754+Nayenso@users.noreply.github.com>
2024-10-17 15:31:16 +0900

    Title: 대문자 소문자 바꾸기, Time: 146ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241017/대문자 소문자 바꾸기/change-uppercase-and-lowercase.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+
+        for(int i=0; i<str.length(); i++){
+            if(Character.isUpperCase(str.charAt(i))){
+                System.out.print(Character.toLowerCase(str.charAt(i)));
+            }else if(Character.isLowerCase(str.charAt(i))){
+                System.out.print(Character.toUpperCase(str.charAt(i)));
+            }
+        }
+    }
+}
