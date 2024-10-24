f428009e24b0854a3f64ac9d2356956577cf7ef7
Code <139698754+Nayenso@users.noreply.github.com>
2024-10-24 17:15:34 +0900

    Title: 대각선으로 숫자 채우기, Time: 248ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 241024/대각선으로 숫자 채우기/diagonal-numbering.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] arr = new int[n][m];
+        int cnt = 1;
+
+        for(int i=0; i<=n+m-2; i++){
+            for(int j=0; j<n; j++){
+                int k = i-j;
+                if(k>=0 && k<m){
+                    arr[j][k]=cnt++;
+                }
+            }
+        }
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
