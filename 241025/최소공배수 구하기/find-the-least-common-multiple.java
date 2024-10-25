85e733112139826d065e30160333ef29bae7b804
Code <139698754+Nayenso@users.noreply.github.com>
2024-10-25 18:12:54 +0900

    Title: 최소공배수 구하기, Time: 112ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241025/최소공배수 구하기/find-the-least-common-multiple.java
+++ 241025/최소공배수 구하기/find-the-least-common-multiple.java
@@ -2,15 +2,15 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanenr sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
         System.out.print(n*makeNum(n,m));
     }
     
     public static int makeNum(int n, int m){
-        int r = n%m;
-        while(r!=0){
+        int r = 1;
+        while(n%m!=0){
             r = n%m;
             n = m;
             m = r;
