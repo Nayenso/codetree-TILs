65c2a81114a59dd1d84901ccb288d61172b3e732
Code <139698754+Nayenso@users.noreply.github.com>
2024-10-25 15:05:18 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 216ms, Memory: 13MB, Status: Passed - CodeTree

--- 241025/가장 왼쪽에 있는 최댓값/leftmost-max-value.java
+++ 241025/가장 왼쪽에 있는 최댓값/leftmost-max-value.java
@@ -10,29 +10,20 @@ public class Main {
             arr[i] = sc.nextInt();
         }
 
-        int max = Integer.MIN_VALUE;
-        int index = -1;
+        int index = n;
 
-        for(int i=0; i<n; i++){
-            if(max < arr[i]){
-                max = arr[i];
-                index = i;
-            }
-        }
-        System.out.print((index+1)+" ");
+        while(index>0){
+            int max = Integer.MIN_VALUE;
+            int maxIndex = -1;
 
-        while(true){
-            max = Integer.MIN_VALUE;
             for(int i=0; i<index; i++){
                 if(max < arr[i]){
                     max = arr[i];
-                    index = i;
+                    maxIndex = i;
                 }
             }
-            System.out.print((index+1)+" ");
-            if(index==0){
-                break;
-            }
+            System.out.print((maxIndex+1)+" ");
+            index = maxIndex;
         }
     }
 }
