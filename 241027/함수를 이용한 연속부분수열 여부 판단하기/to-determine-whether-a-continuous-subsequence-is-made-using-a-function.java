5ea43ae9d28c5162dc1b3fb29e7ae66818f9f999
Code <139698754+Nayenso@users.noreply.github.com>
2024-10-27 23:40:20 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 165ms, Memory: 10MB, Status: Passed - CodeTree

--- 241027/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.java
+++ 241027/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.java
@@ -8,36 +8,37 @@ public class Main {
         int[] A = new int[n1];
         int[] B = new int[n2];
 
-        for(int i=0; i<n1; i++){
+        for (int i = 0; i < n1; i++) {
             A[i] = sc.nextInt();
         }
         
-        for(int i=0; i<n2; i++){
+        for (int i = 0; i < n2; i++) {
             B[i] = sc.nextInt();
         }
-        boolean re = isContain(A,B);
-        if(re){
+        
+        boolean re = isContain(A, B);
+        if (re) {
             System.out.print("Yes");
-        }else{
+        } else {
             System.out.print("No");
         }
     }
-    public static boolean isContain(int[] A, int[] B){
+
+    public static boolean isContain(int[] A, int[] B) {
         int s1 = A.length, s2 = B.length;
 
-        for(int i=0; i<=s1-s2; i++){
+        for (int i = 0; i <= s1 - s2; i++) {
             boolean temp = true;
-            if(A[i]==B[0]){
-                for(int j=0; j<s2; j++){
-                    if(A[i+j]!=B[j]){
-                        temp = false;
-                    }
+            for (int j = 0; j < s2; j++) {
+                if (A[i + j] != B[j]) {
+                    temp = false;
+                    break;
                 }
             }
-            if(temp == false){
-                return false;
+            if (temp) {
+                return true; // 부분 배열이 일치하면 true 반환
             }
         }
-        return true;
+        return false; // 부분 배열이 없는 경우 false 반환
     }
 }
