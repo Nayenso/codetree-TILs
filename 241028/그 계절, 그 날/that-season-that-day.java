716751ee18d6848fc05d6e1441f619098160e499
Code <139698754+Nayenso@users.noreply.github.com>
2024-10-28 14:53:28 +0900

    Title: 그 계절, 그 날, Time: 114ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241028/그 계절, 그 날/that-season-that-day.java
+++ 241028/그 계절, 그 날/that-season-that-day.java
@@ -22,7 +22,13 @@ public class Main {
     }
 
     public static boolean isUnYear(int year){
-        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+        if(year%4==0 && year%100==0){
+            return false;
+        }
+        if(((year%4==0 && year%100==0) || year%400==0)){
+            return true;
+        }
+        return false;
     }
 
     public static int isMonth(int year, int month){
