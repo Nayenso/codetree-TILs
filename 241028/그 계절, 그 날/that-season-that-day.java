cf1da8d0ebd26c9f3e681d8de4f8f51bf188ba0a
Code <139698754+Nayenso@users.noreply.github.com>
2024-10-28 14:46:20 +0900

    Title: 그 계절, 그 날, Time: 118ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241028/그 계절, 그 날/that-season-that-day.java
+++ 241028/그 계절, 그 날/that-season-that-day.java
@@ -25,7 +25,7 @@ public class Main {
         if(year%4==0){
             return true;
         }
-        if((year%4==0 && year%100==0 && year%400==0)){
+        if((year%4==0 && year%100==0 || year%400==0)){
             return true;
         }
         if(year%4==0 && year%100==0){
