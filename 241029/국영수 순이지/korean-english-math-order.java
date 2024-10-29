384ac99718edcf5ead23343e206d30c60f3913c3
Code <139698754+Nayenso@users.noreply.github.com>
2024-10-29 22:48:34 +0900

    Title: 국영수 순이지, Time: 122ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241029/국영수 순이지/korean-english-math-order.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+class grade implements Comparable<grade>{
+    String name;
+    int kor;
+    int eng;
+    int math;
+
+    public grade(String n, int k, int e, int m){
+        name = n;
+        kor = k;
+        eng = e;
+        math = m;
+    }
+
+    @Override
+    public int compareTo(grade temp){
+        if(this.kor == temp.kor){
+            if(this.eng == temp.eng){
+                return this.math - temp.math;
+            }else{
+                return this.eng - temp.eng;
+            }
+        }
+        return this.kor - temp.kor;
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        grade[] arr = new grade[n];
+
+        for(int i=0; i<n; i++){
+            String na =sc.next();
+            int ko = sc.nextInt();
+            int en = sc.nextInt();
+            int ma = sc.nextInt();
+            arr[i] = new grade(na, ko,en,ma);
+        }
+
+        Arrays.sort(arr);
+
+        for(int i=0; i<n; i++){
+            System.out.println(arr[i].name+" "+arr[i].kor+" "+arr[i].eng+" "+arr[i].math);
+        }
+    }
+}
