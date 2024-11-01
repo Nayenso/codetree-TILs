3127a7e4bec21dfe34e55d1555ca4917a164d980
Code <139698754+Nayenso@users.noreply.github.com>
2024-11-01 15:44:30 +0900

    Title: Date to Date, Time: 115ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241101/Date to Date/date-to-date.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m1 = sc.nextInt();
+        int d1 = sc.nextInt();
+        int m2 = sc.nextInt();
+        int d2 = sc.nextInt();
+                        //1   2   3   4   5   6   7   8   9   10  11 12
+        int[] days = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int answer = days[m1] - d1 + d2 + 1;
+        for(int i=m1+1; i<m2; i++){
+            answer += days[i];
+        }
+        System.out.print(answer);
+    }
+}
