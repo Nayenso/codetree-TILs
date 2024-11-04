76efedb5b83b9401c98098ca74b90d522a676f95
Code <139698754+Nayenso@users.noreply.github.com>
2024-11-04 14:03:23 +0900

    Title: 정수 명령 처리 5, Time: 331ms, Memory: 20MB, Status: Passed - CodeTree

--- 241104/정수 명령 처리 5/process-numeric-commands-5.java
+++ 241104/정수 명령 처리 5/process-numeric-commands-5.java
@@ -4,20 +4,22 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        sc.nextLine();
         ArrayList<Integer> arr = new ArrayList<>();
 
         for(int i=0; i<n; i++){
-            String op = sc.next();
-            int item = sc.nextInt();
-
-            if(op.equals("push_back")){
-                arr.add(item);
-            }else if(op.equals("pop_back")){
+            String temp = sc.nextLine();
+            String[] op = temp.split(" ");
+            
+            if(op[0].equals("push_back")){
+                arr.add(Integer.valueOf(op[1]));
+            }else if(op[0].equals("pop_back")){
                 arr.remove(arr.size()-1);
-            }else if(op.equals("size")){
+            }else if(op[0].equals("size")){
                 System.out.println(arr.size());
-            }else if(op.equals("get")){
-                System.out.println(arr.get(item));
+            }else if(op[0].equals("get")){
+                int index = Integer.parseInt(op[1]) - 1;
+                System.out.println(arr.get(index));
             }
         }
     }
