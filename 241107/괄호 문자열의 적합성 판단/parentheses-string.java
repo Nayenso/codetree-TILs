ac4648f2cf8745fc34306ce89d1657d13baee2a2
Code <139698754+Nayenso@users.noreply.github.com>
2024-11-07 22:22:24 +0900

    Title: 괄호 문자열의 적합성 판단, Time: 108ms, Memory: 10MB, Status: Runtime Error - CodeTree

---
+++ 241107/괄호 문자열의 적합성 판단/parentheses-string.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String word = sc.next();
+        if(flag(word)){
+            System.out.print("Yes");
+        }else{
+            System.out.print("No");
+        }
+    }
+
+    public static boolean flag(String wd){
+        Stack<Character> s = new Stack<>();
+        int len = wd.length();
+
+        for(int i=0; i<len; i++){
+            char tm = wd.charAt(i);
+
+            if(tm=='('){
+                if(s.size()==len){
+                    System.out.print("error");
+                }
+                s.push(tm);
+            }else{
+                if(s.size()==0){
+                    System.out.print("error");
+                }
+                s.pop();
+            }
+        }
+        if(!s.isEmpty()){
+            return false;
+        }
+        return true;
+    }
+}
