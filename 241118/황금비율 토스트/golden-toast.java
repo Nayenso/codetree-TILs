56fa4a190cba1ad48bbf4e2c8d0ec19045484e28
Code <139698754+Nayenso@users.noreply.github.com>
2024-11-18 17:12:08 +0900

    Title: 황금비율 토스트, Time: 125ms, Memory: 10MB, Status: Runtime Error - CodeTree

---
+++ 241118/황금비율 토스트/golden-toast.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), m = sc.nextInt();
+        String word = sc.next();
+        LinkedList<Character> wd = new LinkedList<>();
+        
+        for(int i=0; i<m; i++){
+            char c = word.charAt(i);
+            wd.addLast(c);
+        }
+
+        ListIterator li = wd.listIterator(wd.size());
+
+        for(int i=0; i<n; i++){
+            char op = sc.next().charAt(0);
+            if(op=='L'){
+                if(li.hasPrevious()){
+                    li.previous();
+                }
+            }else if(op=='R'){
+                if(li.hasNext()){
+                    li.next();
+                }
+            }else if(op=='D'){
+                if(li.hasNext()){
+                    li.next();
+                    li.remove();
+                }
+            }else if(op=='P'){
+                char c1 = sc.next().charAt(0);
+                li.add(c1);
+            }
+        }
+        li = wd.listIterator();
+        while(li.hasNext()){
+            System.out.print(li.next());
+        }
+    }
+}
