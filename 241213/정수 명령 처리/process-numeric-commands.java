dd162c4a36b3ea913f1aace36f614bb078293de2
Code <139698754+Nayenso@users.noreply.github.com>
2024-12-13 00:21:24 +0900

    Title: 정수 명령 처리, Time: 316ms, Memory: 17MB, Status: Passed - CodeTree

---
+++ 241213/정수 명령 처리/process-numeric-commands.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Stack<Integer> st = new Stack<>();
+
+        for(int i=0; i<n; i++){
+            String op = sc.next();
+            if(op.equals("push")){
+                int a = sc.nextInt();
+                st.push(a);
+            }else if(op.equals("pop")){
+                System.out.println(st.pop());
+            }else if(op.equals("size")){
+                System.out.println(st.size());
+            }else if(op.equals("empty")){
+                System.out.println(st.isEmpty()?1:0);
+            }else if(op.equals("top")){
+                System.out.println(st.peek());
+            }
+        }
+    }
+}
