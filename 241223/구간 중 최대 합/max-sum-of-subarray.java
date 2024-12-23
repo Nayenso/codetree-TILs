5800aa7c79106570917e3922c8da09fe29604cfc
Code <139698754+Nayenso@users.noreply.github.com>
2024-12-23 23:10:33 +0900

    Title: 구간 중 최대 합, Time: 131ms, Memory: 10MB, Status: Runtime Error - CodeTree

---
+++ 241223/구간 중 최대 합/max-sum-of-subarray.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), k = sc.nextInt();
+        int max = Integer.MIN_VALUE;
+        int[] arr = new int[n];
+
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        for(int i=0; i<=n-k; i++){
+            int sum = 0;
+            for(int j=i; j<=i+k; j++){
+                sum+=arr[j];
+            }
+            if(sum>max){
+                max = sum;
+            }
+        }
+        System.out.print(max);
+    }
+}
