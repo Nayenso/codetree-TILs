1ec04a834cfd86b7b07359cab64721573fe8b876
Code <139698754+Nayenso@users.noreply.github.com>
2025-09-17 16:28:52 +0900

    Title: 숫자 피라미드, Time: 128ms, Memory: 10MB, Status: Passed - Codetree

--- 250917/숫자 피라미드/number-pyramid.java
+++ 250917/숫자 피라미드/number-pyramid.java
@@ -6,8 +6,8 @@ public class Main {
         int N = sc.nextInt();
 
         for(int i=1; i<=N; i++){
-            for(int j=1; j<=N; j++){
-                System.out.print(j+" ");
+            for(int j=i; j<=i*2-1; j++){
+                System.out.print(i+" ");
             }
             System.out.println();
         }
